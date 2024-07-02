9c118cfbca15c93cb4d65891060c278077e13338
jung6A <heide6@naver.com>
2024-07-02 12:20:26 +0900

    java

--- JavaClass/src/java0702_2/ClassObject.java
+++ JavaClass/src/java0702_2/ClassObject.java
@@ -4,6 +4,8 @@ public class ClassObject {
 
 	public static void main(String[] args) {
 		
+		System.out.println(Car.maker);
+		
 		Car car1;
 		
 		car1=new Car(); //carName, displacement, fuel 변수 생성
@@ -18,6 +20,7 @@ public class ClassObject {
 //클래스 변수, 인스턴스 변수
 //인스턴스 변수는 생성시 메모리 할당이 됨
 //클래스 객체 생성은 new 연산자를 통함
+//클래스 변수는 프로그램 실행 시 메모리에 로드하며 생성됨
 
 class Car{
 	static String maker="현대"; //클래스 변수
